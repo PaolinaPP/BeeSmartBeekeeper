c37953c8b4a4adffefe6d835fcec2168e990df79
PaolinaPP <paolina12123@gmail.com>
2017-03-07 23:58:28 +0200

    Add files via upload

---
+++ BeeSmartBeekeeper/src/com/beeSmartBeekeeper/AuthenticationFilter.java
@@ -0,0 +1,72 @@
+package com.beeSmartBeekeeper.servlets.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AuthenticationFilter
+ */
+@WebFilter("/AuthenticationFilter")
+public class AuthenticationFilter implements Filter {
+	private ServletContext context;
+
+    /**
+     * Default constructor. 
+     */
+    public AuthenticationFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		String uri = req.getRequestURI();
+		this.context.log("Requested Resource::"+uri);
+		
+		HttpSession session = req.getSession(false);
+		
+		if(session == null && !(uri.endsWith("html") || uri.endsWith("LoginServlet"))){
+			this.context.log("Unauthorized access request");
+			res.sendRedirect("login.html");
+		}else{
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		}
+		
+		
+	}
+
+	
+
+	public void destroy() {
+		//close any resources here
+	}
+
+	
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		this.context = fConfig.getServletContext();
+		this.context.log("AuthenticationFilter initialized");
+	}
+
+}
---
+++ BeeSmartBeekeeper/src/com/beeSmartBeekeeper/RequestLoggingFilter.java
@@ -0,0 +1,67 @@
+package com.beeSmartBeekeeper.servlets.filters;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class RequestLoggingFilter
+ */
+@WebFilter("/RequestLoggingFilter")
+public class RequestLoggingFilter implements Filter {
+	private ServletContext context;
+
+    /**
+     * Default constructor. 
+     */
+    public RequestLoggingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+	
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		Enumeration<String> params = req.getParameterNames();
+		while(params.hasMoreElements()){
+			String name = params.nextElement();
+			String value = request.getParameter(name);
+			this.context.log(req.getRemoteAddr() + "::Request Params::{"+name+"="+value+"}");
+		}
+		
+		Cookie[] cookies = req.getCookies();
+		if(cookies != null){
+			for(Cookie cookie : cookies){
+				this.context.log(req.getRemoteAddr() + "::Cookie::{"+cookie.getName()+","+cookie.getValue()+"}");
+			}
+		}
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	public void destroy() {
+		//we can close resources here
+	}
+	
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		this.context = fConfig.getServletContext();
+		this.context.log("RequestLoggingFilter initialized");
+	}
+
+}
